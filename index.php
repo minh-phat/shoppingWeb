c62ccad8989b78bc3aa4d6eec54dd9a3c5eeb14a
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-11-25 17:10:25 +0700

    header

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
 	//$_SESSION['url'] = 'shoppingWeb/';
 	$_SESSION['url'] = 'https://shoppingwebonline.herokupapp.com/';
 	//header('Location: //localhost/shoppingWeb/user/shop.php');
-	header('Location: /user/shop.php');
+	header('Location: user/shop.php');
 	exit;
 ?>

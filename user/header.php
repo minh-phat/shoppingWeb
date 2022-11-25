4ba1a64105d3a87e245dde28ec36cf6acd6fc90d
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-11-25 12:27:08 +0700

    css

--- user/header.php
+++ user/header.php
@@ -75,9 +75,9 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
-							<li class="nav-item <?php echo htmlspecialchars($_SESSION["menuCssHome"]);?>"><a class="nav-link" href="../user/home.php">Trang Chủ</a></li>
-                            <li class="nav-item <?php echo htmlspecialchars($_SESSION["menuCssIndex"]);?>"><a class="nav-link" href="../index.php">Sản Phẩm</a></li>
-							<li class="nav-item <?php echo htmlspecialchars($_SESSION["menuCssLogin"]);?>"><a class="nav-link" href="../user/login.php">Đăng Nhập</a></li>
+							<li class="nav-item <?php echo htmlspecialchars($_SESSION["menuCssHome"]);?>"><a class="nav-link" href="<?php echo htmlspecialchars($_SESSION['url']) ?>user/home.php">Trang Chủ</a></li>
+                            <li class="nav-item <?php echo htmlspecialchars($_SESSION["menuCssIndex"]);?>"><a class="nav-link" href="<?php echo htmlspecialchars($_SESSION['url']) ?>index.php">Sản Phẩm</a></li>
+							<li class="nav-item <?php echo htmlspecialchars($_SESSION["menuCssLogin"]);?>"><a class="nav-link" href="<?php echo htmlspecialchars($_SESSION['url']) ?>user/login.php">Đăng Nhập</a></li>
 							<!-- <li class="nav-item submenu dropdown">
 								<a href="index.php" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sản Phẩm</a>
 								<ul class="dropdown-menu">

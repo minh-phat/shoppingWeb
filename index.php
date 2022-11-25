a5dbb78f6ff2dd6ace4142c3dc9b1e90e526af7c
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-11-25 12:43:43 +0700

    url

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 	session_start();
 	
-	$_SESSION['url'] = 'http://localhost/shoppingWeb/';
+	//$_SESSION['url'] = 'shoppingWeb/';
 	//header('Location: //localhost/shoppingWeb/user/shop.php');
 	header('Location: /user/shop.php');
 	exit;

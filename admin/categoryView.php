bfc6139308685128748622fcdf6c48d9e5befed0
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-11-26 13:49:19 +0700

    add new product anh category

--- admin/categoryView.php
+++ admin/categoryView.php
@@ -7,23 +7,15 @@
               <div class="col-md-6 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title">Thểm Sản Phẩm</h4>
-                    <p class="card-description"> Thông Tin Sản Phẩm Mới </p>
+                    <h4 class="card-title">Thểm Thể Loại</h4>
+                    <p class="card-description"> Thông Tin Thể Loại </p>
                     <form class="forms-sample">
                       <div class="form-group">
-                        <label for="exampleInputName1">Tên Sản Phẩm</label>
+                        <label for="exampleInputName1">Tên Thể Loại</label>
                         <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
                       </div>
                       <div class="form-group">
-                        <label for="exampleInputEmail3">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputPassword4">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
-                      </div>
-                      <div class="form-group">
-                        <label>File upload</label>
+                        <label>Hình Ảnh</label>
                         <input type="file" name="img[]" class="file-upload-default">
                         <div class="input-group col-xs-12">
                           <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
@@ -31,14 +23,6 @@
                             <button class="file-upload-browse btn btn-info" type="button">Upload</button>
                           </span>
                         </div>
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputCity1">City</label>
-                        <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleTextarea1">Textarea</label>
-                        <textarea class="form-control" id="exampleTextarea1" rows="2"></textarea>
                       </div>
                       <button type="submit" class="btn btn-success mr-2">Submit</button>
                       <button class="btn btn-light">Cancel</button>
--- admin/header.php
+++ admin/header.php
@@ -200,12 +200,6 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="productView.php">
-                <i class="menu-icon typcn typcn-document-text"></i>
-                <span class="menu-title">Quản Lý Sản Phẩm</span>
-              </a>
-            </li>
-            <li class="nav-item">
               <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
                 <i class="menu-icon typcn typcn-coffee"></i>
                 <span class="menu-title">Quản Lý Sản Phẩm</span>
@@ -213,6 +207,9 @@
               </a>
               <div class="collapse" id="ui-basic">
                 <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                    <a class="nav-link" href="categoryView.php">Thêm Thể Loại Sản Phẩm</a>
+                  </li>
                   <li class="nav-item">
                     <a class="nav-link" href="productView.php">Thêm Sản Phẩm Mới</a>
                   </li>
@@ -249,12 +246,6 @@
               <a class="nav-link" href="pages/forms/basic_elements.html">
                 <i class="menu-icon typcn typcn-shopping-bag"></i>
                 <span class="menu-title">Form elements</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="dashboard.phpadf">
-                <i class="menu-icon typcn typcn-shopping-bag"></i>
-                <span class="menu-title">Dashboard</span>
               </a>
             </li>
             <li class="nav-item">
